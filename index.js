cf92f0471a1df83dcda468281c324b67c374820f
ArpitBlagan <114584343+ArpitBlagan@users.noreply.github.com>
2023-11-17 20:02:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ mongoose.connect(process.env.URL,{
 const cors=require('cors');
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(cors({origin:[,'*','http://localhost:5173','https://6557756033b7a42d8355017a--scintillating-liger-7a4d4b.netlify.app/']}));app.use(express.json());
+app.use(cors({origin:['https://6557756033b7a42d8355017a--scintillating-liger-7a4d4b.netlify.app']}));app.use(express.json());
 app.use("/api/v1",Router)
 app.listen(process.env.PORT,()=>{console.log(`Listening on port ${process.env.PORT}`);});
